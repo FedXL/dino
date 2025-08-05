f3335d4da8595ecd9a9e2602b022822dde2b56a3
fedor <fedorkuruts@gmail.com>
2025-08-06 04:40:13 +0500

    experiment

--- expexp.py
+++ expexp.py
@@ -1,19 +1,14 @@
 import torch
 from PIL import Image
 from transformers import AutoModel, CLIPImageProcessor
-import requests
-from io import BytesIO
-
-url = 'https://storage.googleapis.com/master_base_bucket/building_571_a89cb34dbc8f4ac1bae484419d1eba9a.jpg'  # Замените на нужный URL
-response = requests.get(url)
-image = Image.open(BytesIO(response.content)).convert('RGB')
 
 model = AutoModel.from_pretrained(
     'OpenGVLab/InternViT-300M-448px-V2_5',
     torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
-    trust_remote_code=True
-).cuda().eval()
+    trust_remote_code=True).cuda().eval()
+
+image = Image.open('./1.png').convert('RGB')
 
 image_processor = CLIPImageProcessor.from_pretrained('OpenGVLab/InternViT-300M-448px-V2_5')
 
