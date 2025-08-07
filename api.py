e141d0a5904909bffa3044e0eb8c46fdaf136c86
fedor <fedorkuruts@gmail.com>
2025-08-08 04:10:20 +0500

    some

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from starlette.concurrency import run_in_threadpool
-from embedding_handler import Dino2ExtractorV1, EmbeddingService, URLImageLoader
+from embedding_handler import Dino2ExtractorV1, EmbeddingService, URLImageLoader, InternVIT600mbExtractor
 
 from dotenv import load_dotenv
 
@@ -20,7 +20,7 @@ class EmbeddingRequest(BaseModel):
 results = {}
 AUTH_TOKEN = os.getenv('TOKEN')
 embedding_service = EmbeddingService(URLImageLoader(), Dino2ExtractorV1())
-
+embedding_vit_600m = EmbeddingService(URLImageLoader(), InternVIT600mbExtractor())
 
 
 
@@ -75,3 +75,51 @@ async def root():
 
 
 
+
+embedding_semaphore = asyncio.Semaphore(1)  # максимум 1 запрос к модели одновременно
+
+# 💬 Запрос
+class EmbeddingRequest(BaseModel):
+    url: str
+
+# 💬 FastAPI
+
+
+@app.post("/embedding/test_extract")
+async def extract_embedding(request: EmbeddingRequest):
+    start = time.perf_counter()
+    print(f"\n[{request.url}] 🌐 Запрос получен")
+
+    # 🔄 1. Параллельно загружаем изображение
+    try:
+        image, message = embedding_vit_600m.loader.load(request.url)
+        if image is None:
+            raise ValueError(message)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    loaded = time.perf_counter()
+    print(f"[{request.url}] ✅ Изображение загружено за {loaded - start:.2f} сек")
+
+    try:
+        async with asyncio.timeout(10):  # таймаут ожидания очереди
+            queue_start = time.perf_counter()
+            print(f"[{request.url}] ⏳ Ожидаем доступ к модели...")
+
+            async with embedding_semaphore:
+                waited = time.perf_counter()
+                print(f"[{request.url}] 🔓 Доступ получен через {waited - queue_start:.2f} сек")
+
+                # 💡 3. Извлекаем эмбеддинг
+                result = embedding_vit_600m.extractor.extract(image)
+                embedding = result.tolist()
+
+                finished = time.perf_counter()
+                print(f"[{request.url}] 🧠 Обработка завершена за {finished - waited:.2f} сек")
+    except TimeoutError:
+        raise HTTPException(status_code=503, detail="Модель занята. Повторите позже.")
+
+    total = time.perf_counter()
+    print(f"[{request.url}] ✅ Общая длительность: {total - start:.2f} сек")
+
+    return {"embedding": embedding, "url": request.url}
